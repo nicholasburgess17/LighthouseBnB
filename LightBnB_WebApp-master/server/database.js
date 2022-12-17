4ae3e33e9ddbc833cb57466d1daf7829411d8c98
Nicholas Burgess <Nicholas.Burgess13@gmail.comgit config --global user.name nicholas>
2022-12-17 23:37:45 +0000

    refactor addProperty function

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -175,10 +175,36 @@ exports.getAllProperties = getAllProperties;
  * @param {{}} property An object containing all of the property details.
  * @return {Promise<{}>} A promise to the property.
  */
-const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+ const addProperty = function (property) {
+  const queryString = `
+  INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms) 
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+  RETURNING *;
+  `;
+
+  const values = [
+    properties.owner_id,
+    properties.title,
+    properties.description,
+    properties.thumbnail_photo_url,
+    properties.cover_photo_url,
+    properties.cost_per_night,
+    properties.street,
+    properties.city,
+    properties.province,
+    properties.post_code,
+    properties.country,
+    properties.parking_spaces,
+    properties.number_of_bathrooms,
+    properties.number_of_bedrooms,
+  ];
+
+  return pool
+    .query(queryString, values)
+    .then((result) => {
+      console.log(result.rows[0]);
+      return result.rows[0];
+    })
+    .catch((err) => console.log(err.message));
 };
 exports.addProperty = addProperty;
